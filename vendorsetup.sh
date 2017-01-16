12fc64e0a7b0d9c8e005c71be212d412356e5a8e
grace5921 <androidlover5842@gmail.com>
2017-01-16 15:26:32 +0530

    typi

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,11 +1,11 @@
-add_lunch_combo omni_on7xelte-eng
-function mkazip_on7xelte
+add_lunch_combo omni_zenlteatt-eng
+function mkazip_zenlteatt
 {
-zip META-INF.zip -r device/samsung/on7xelte/META-INF 
-mv META-INF.zip out/target/product/on7xelte/META-INF.zip
-cd out/target/product/on7xelte/
+zip META-INF.zip -r device/samsung/zenlteatt/META-INF 
+mv META-INF.zip out/target/product/zenlteatt/META-INF.zip
+cd out/target/product/zenlteatt/
 unzip META-INF.zip 
 rm META-INF.zip 
-zip twrp-$(date +%Y%m%d)-3.0.3-UNOFFICIAL-on7xelte.zip  -r recovery.img -r META-INF
+zip twrp-$(date +%Y%m%d)-3.0.3-UNOFFICIAL-zenlteatt.zip  -r recovery.img -r META-INF
 cd ../../../../
 }
